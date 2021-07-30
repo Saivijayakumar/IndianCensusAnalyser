d25de1cc51b99dcdcdc2188647c066c896a5d6f3
Sai Vijayakumar Bollu <saivijayakumar.b@tvsnext.io>
2021-07-30 12:52:02 +0530

    [SaiVijayaKumar]Indian State Code Validation

---
+++ IndianCensusAnalyser/IndianCensusAdapter.cs
@@ -0,0 +1,45 @@
+﻿using IndianCensusAnalyser.DataDAO;
+using IndianCensusAnalyser.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndianCensusAnalyser
+{
+    public class IndianCensusAdapter:CensusAdapter
+    {
+        string[] censusData;
+        Dictionary<string, CensusDTO> datamap;
+        public Dictionary<string, CensusDTO> LoadCensusData(string csvFilePath,string dataHeaders)
+        {
+            try
+            {
+                datamap = new Dictionary<string, CensusDTO>();
+                censusData = GetCensusData(csvFilePath, dataHeaders);
+                foreach (string data in censusData.Skip(1))
+                {
+                    if (!data.Contains(","))
+                    {
+                        throw new CensusAnalyserException("File Containers Wrong Delimiter", CensusAnalyserException.ExceptionType.INCOREECT_DELIMITER);//
+                    }
+                    string[] coloumn = data.Split(',');
+                    if (csvFilePath.Contains("IndianStateCode.csv"))
+                    {
+                        datamap.Add(coloumn[1], new CensusDTO(new StateCodeDataDAO(coloumn[0], coloumn[1], coloumn[2], coloumn[3])));
+                    }
+                    if (csvFilePath.Contains("IndiaStateCensusData.csv"))
+                    {
+                        datamap.Add(coloumn[1], new CensusDTO(new CensusDataDAO(coloumn[0], coloumn[1], coloumn[2], coloumn[3])));
+                    }
+                }
+                return datamap;
+            }
+            catch(CensusAnalyserException ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
