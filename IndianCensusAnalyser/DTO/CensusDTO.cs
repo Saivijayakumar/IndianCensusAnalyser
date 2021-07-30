86dc99c5c2f00ee23126be825904aa43ad4c955f
Sai Vijayakumar Bollu <saivijayakumar.b@tvsnext.io>
2021-07-30 12:52:02 +0530

    [SaiVijayaKumar] Commiting Final code

---
+++ IndianCensusAnalyser/DTO/CensusDTO.cs
@@ -0,0 +1,40 @@
+﻿using IndianCensusAnalyser.DataDAO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndianCensusAnalyser.DTO
+{
+    public class CensusDTO
+    {
+        public int serialNumber;
+        public string stateName;
+        public int tin;
+        public string stateCode;
+        public string state;
+        public long population;
+        public long area;
+        public long density;
+        public double totalArea;
+        public double waterArea;
+        public double landArea;
+
+        public CensusDTO(StateCodeDataDAO stateCodeDataDAO)
+        {
+            this.serialNumber = stateCodeDataDAO.serialNumber;
+            this.stateName = stateCodeDataDAO.stateName;
+            this.tin = stateCodeDataDAO.tin;
+            this.stateCode = stateCodeDataDAO.stateCode;
+        }
+
+        public CensusDTO(CensusDataDAO censusDataDAO)
+        {
+            this.state = censusDataDAO.state;
+            this.population = censusDataDAO.population;
+            this.area = censusDataDAO.area;
+            this.density = censusDataDAO.density;
+        }
+    }
+}
